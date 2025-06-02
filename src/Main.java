c121c39eff98089d45043565e8e2cdfdd093b294
Gustavo Morais <89202436+GustavoMdancers@users.noreply.github.com>
2025-06-01 21:40:19 -0300

    Testes

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         Aluno aluno1 = new Aluno("João Silva", "123456", "Engenharia");
         // Adicionar o aluno ao arquivo CSV
         AlunoCSV.adicionarAluno(aluno1);
-
+        Professor professor1 = new Professor ("Flavio", "643197", "ADS");
+        ProfessorCSV.adicionarProfessor(professor1);
     }
 }
